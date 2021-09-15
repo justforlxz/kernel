7e806da19021e05339c94c71065d46a8d0e8811b
justforlxz <justforlxz@gmail.com>
2021-09-15 13:38:04 +0800

    integration tests

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,16 @@
 #![no_main]
 #![feature(llvm_asm)]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(kernel::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
-mod serial;
-mod vga_buffer;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-  Success = 0x10,
-  Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-  use x86_64::instructions::port::Port;
-  unsafe {
-    let mut port = Port::new(0xf4);
-    port.write(exit_code as u32);
-  }
-}
+use kernel::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
   println!("Hello World{}", "!");
-  vga_buffer::test();
+
   #[cfg(test)]
   test_main();
 
@@ -38,24 +21,12 @@ pub extern "C" fn _start() -> ! {
 #[cfg(not(test))] // new attribute
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-  println!("{}", info);
+  println!("{}", _info);
   loop {}
 }
 
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-  serial_println!("[failed]\n");
-  serial_println!("Error: {}\n", info);
-  exit_qemu(QemuExitCode::Failed);
-  loop {}
-}
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-  serial_println!("Running {} tests", tests.len());
-  for test in tests {
-    test();
-  }
-  exit_qemu(QemuExitCode::Success);
+  kernel::test_panic_handler(info)
 }
---
+++ tests/basic_boot.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(kernel::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use kernel::{println, serial_print, serial_println};
+
+#[test_case]
+fn test_println() {
+    serial_print!("test_println... ");
+    println!("test_println output");
+    serial_println!("[ok]");
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    kernel::test_panic_handler(info)
+}
