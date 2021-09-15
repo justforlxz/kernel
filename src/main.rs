00bc3e458ddd4b6ee4928e71e041bdb7ca150363
justforlxz <justforlxz@gmail.com>
2021-09-15 13:24:51 +0800

    add print and unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,61 @@
 #![no_std]
 #![no_main]
 #![feature(llvm_asm)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
+mod serial;
+mod vga_buffer;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+  Success = 0x10,
+  Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+  use x86_64::instructions::port::Port;
+  unsafe {
+    let mut port = Port::new(0xf4);
+    port.write(exit_code as u32);
+  }
+}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-    loop {}
+  println!("Hello World{}", "!");
+  vga_buffer::test();
+  #[cfg(test)]
+  test_main();
+
+  loop {}
 }
 
+#[cfg(not(test))] // new attribute
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+  println!("{}", info);
+  loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+  serial_println!("[failed]\n");
+  serial_println!("Error: {}\n", info);
+  exit_qemu(QemuExitCode::Failed);
+  loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+  serial_println!("Running {} tests", tests.len());
+  for test in tests {
+    test();
+  }
+  exit_qemu(QemuExitCode::Success);
 }
---
+++ src/serial.rs
@@ -0,0 +1,42 @@
+use lazy_static::lazy_static;
+use spin::Mutex;
+use uart_16550::SerialPort;
+
+lazy_static! {
+  pub static ref SERAL1: Mutex<SerialPort> = {
+    let mut serial_port = unsafe { SerialPort::new(0x3F8) };
+    serial_port.init();
+    Mutex::new(serial_port)
+  };
+}
+
+#[doc(hidden)]
+pub fn _print(args: ::core::fmt::Arguments) {
+  use core::fmt::Write;
+  SERAL1
+    .lock()
+    .write_fmt(args)
+    .expect("Printing to serial failed");
+}
+
+#[macro_export]
+macro_rules! serial_print {
+  ($($arg:tt)*) => {
+    $crate::serial::_print(format_args!($($arg)*));
+  };
+}
+
+#[macro_export]
+macro_rules! serial_println {
+  () => ($crate::serial_print!("\n"));
+  ($fmt:expr) => ($crate::serial_print!(concat!($fmt, "\n")));
+  ($fmt:expr, $($arg:tt)*) => ($crate::serial_print!(
+      concat!($fmt, "\n"), $($arg)*));
+}
+
+#[test_case]
+fn trivial_assertion() {
+  serial_print!("trivial assertion... ");
+  assert_eq!(1, 1);
+  serial_println!("[ok]");
+}
